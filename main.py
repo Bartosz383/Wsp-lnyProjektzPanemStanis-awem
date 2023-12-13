a56fd9d390394e035ef6cd44c38a128d06669400
Bartosz Kruszyński <62513514+Bartosz383@users.noreply.github.com>
2023-12-13 20:33:08 +0100

    Odszyfrowuje szyfr cezara

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-print("Hej")
+def odszyfruj(wiadomosc, przesuniecie):
+  odszyfrowana = ""
+  for znak in wiadomosc:
+    if znak.isalpha():
+      odszyfrowana += chr((ord(znak) - przesuniecie) % 26 + ord("A"))
+    else:
+      odszyfrowana += znak
+  return odszyfrowana
 
-a = 4
-b = 8
-c = 23
+wiadomosc = "QWERTYUIOPASDFGHJKLZXCVBNM"
+przesuniecie = 3
 
-print(a+b)
-
-print(a + b + c / 2)
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-xpoints = np.array([1, 8])
-ypoints = np.array([3, 10])
-
-plt.plot(xpoints, ypoints)
-plt.show()
+print(odszyfruj(wiadomosc, przesuniecie))
