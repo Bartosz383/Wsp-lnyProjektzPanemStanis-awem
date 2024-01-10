3c146a47c9483c3c765e8de58a1111580b304c75
Bartosz Kruszyński <62513514+Bartosz383@users.noreply.github.com>
2024-01-10 20:53:58 +0100

    Optymalizacja algorytmu i poprawa działania

--- main.py
+++ main.py
@@ -100,9 +100,10 @@
 # root.mainloop()
 
 
-from time import sleep
+import time
 import sys
 import random
+import threading
 # lista_klawiszy = {1: 'q', 2: 'w', 3: 'e', 4: 'r', 5: 't', 6: 'y', 7: 'u', 8: 'i', 9: 'o', 10: 'p',
 #                  11: 'a', 12: 's', 13: 'd', 14: 'f', 15: 'g', 16: 'h', 17: 'j', 18: 'k', 19: 'l',
 #                  20: 'z', 21: 'x', 22: 'c', 23: 'v', 24: 'b', 25: 'n', 26: 'm'}
@@ -110,7 +111,71 @@ lista_klawiszy = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"]
 klawisz_wygranej = random.choice(lista_klawiszy)
 wybrane_miejsca = random.choice(lista_klawiszy)
 ilosc_prob = 10
-def head():
+
+class CountdownThread(threading.Thread):
+    def __init__(self, start_value):
+        super(CountdownThread, self).__init__()
+        self.start_value = start_value
+
+    def run(self):
+        for i in range(self.start_value, 0, -1):
+            print(i)
+            time.sleep(1)
+def czas():
+    global start_time
+    czasomierz = 5
+    while czasomierz > 0:
+        elapsed_time = int(time.time() - start_time)
+        czasomierz = 5 - elapsed_time
+        print(f"Czas: {czasomierz} sekund")
+
+        if czasomierz == 0:
+         print('Koniec czasu, przegrałeś!')
+         exit(99)
+    # czasomierz = czasomierz - 1
+    # sleep(1)
+    # print(f"Czas: {czasomierz} sekund")
+    # if czasomierz == 0:
+    #     print('Koniec czasu, przegrałeś!')
+    #     exit(99)
+
+
+def wpis():
+    print("\n" * 100)
+    # sleep(3)
+    print("Start!")
+    # sleep(2)
+    print("\n" * 100)
+    print(klawisz_wygranej)
+    print(wybrane_miejsca)
+    print(lista_klawiszy)
+
+
+def ilosc_prod():
+    ilosc_prob = 10
+
+    while True:
+        wpis_gracza = input("Wpisz literę: ")
+
+        if wpis_gracza == wybrane_miejsca:
+            ilosc_prob = ilosc_prob - 1
+            print(f"Pozostałe próby: {ilosc_prob}")
+            print("Wpadłeś w bombę klawiaturową, Przegrałeś!")
+            break
+
+        elif wpis_gracza != klawisz_wygranej:
+            ilosc_prob = ilosc_prob - 1
+            print("Źle! Próbuj dalej!")
+            print(f"Pozostałe próby: {ilosc_prob}")
+
+            if ilosc_prob == 0:
+                print("Skończyły ci się próby, Przegrałeś!")
+                break
+
+        elif wpis_gracza == klawisz_wygranej:
+            print("Brawo, znalazłeś wygrany klawisz!")
+            break
+def main():
     line_1 = "Witaj w klawiaturowym saperze!"
     while True:
         def efekt_pisania():
@@ -119,7 +184,7 @@ def head():
                 #sys.stdout.flush()
                 #sleep(0.1)
         efekt_pisania()
-        sleep(1.5)
+        #sleep(1.5)
         line_1 = "\nWpisz literę S aby zacząć: Literę Z żeby zobacyć zasady gry: Literę W aby wyjść z gry.\n"
         efekt_pisania()
 
@@ -127,53 +192,11 @@ def head():
         if line_1_input == "S":
             line_1 = "\n\nGra rozpoczyna się......"
             efekt_pisania()
-            def rozgrywka():
-                def przechodzenie_gry():
-                    def wpis():
-                        print("\n" * 100)
-                        sleep(3)
-                        print("Start!")
-                        sleep(2)
-                        print("\n" * 100)
-                        print(klawisz_wygranej)
-                        print(wybrane_miejsca)
-                        print(lista_klawiszy)
-                    def czas():
-                        czasomierz = 60
-                        while czasomierz > 0:
-                            czasomierz = czasomierz - 1
-                            sleep(1)
-                            ilosc_prob = 10
-                            print(f"Czas: {czasomierz} sekund")
-                            wpis_gracza = input("Wpisz literę: ")
 
-                            if wpis_gracza != klawisz_wygranej and wpis_gracza != klawisz_wygranej:
-                                ilosc_prob = ilosc_prob - 1
-                                print("Źle! Próbuj dalej!")
-                                print(f"Pozostałe próby: {ilosc_prob}")
+            wpis()
+            ilosc_prod()
+            break
 
-                                if ilosc_prob == 0:
-                                    print("Skończyły ci się próby, Przegrałeś!")
-                                    exit(99)
-
-                            elif wpis_gracza == wybrane_miejsca:
-                                print("Wpadłeś w bombę klawiaturową, Przegrałeś!")
-                                exit(99)
-
-                            elif wpis_gracza == klawisz_wygranej:
-                                print("Brawo, znalazłeś wygrany klawisz!")
-                                break
-
-                        if czasomierz == 0:
-                            print('Koniec czasu, przegrałeś!')
-                            exit(99)
-
-
-                    wpis()
-                    czas()
-                przechodzenie_gry()
-            rozgrywka()
-            exit(99)
         elif line_1_input == "Z":
             print("Klawiaturowy Saper: Zasady Gry: \nW tej grze, musisz odgadnąć przez komputer wybrany klawisz litery na twojej klawiaturze"
                   "\nJednak masz na to tylko 60 sekund czyli inaczej minutę aby odgadnąć dany klawisz"
@@ -182,10 +205,12 @@ def head():
                   "\nKomputer będzie zdolny ci dawać wskazówki. Będzie on także zdolny do mówienia czy twoja próba jest daleko, blisko bądź bardzo blisko do celu."
                   "\nZakres klawiszy jest od q do m. (Tylko alfabet)"
                   "\n\n Powodzenia!")
-            exit(99)
+            break
         elif line_1_input == "W":
             print("Do Zobaczenia!")
-            exit(99)
+            break
         else:
             print("Przepraszamy, lecz nie znaleziono takiej komendy.")
-head()
+
+if __name__ == "__main__":
+    main()
